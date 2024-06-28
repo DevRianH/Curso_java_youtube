2e5ba97c23deb54cfad39cf51fe3f9e82abcf4d9
Rian H. S. Ezequias <rhezequias@coderp.ribeiraopreto.sp.gov.br>
2024-06-28 13:57:23 -0300

    feito até a aula 39

--- src/main/java/com/rian/cursojava/aula39/Pessoa.java
+++ src/main/java/com/rian/cursojava/aula39/Pessoa.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.rian.cursojava.aula38;
+package com.rian.cursojava.aula39;
 
 /**
  *
@@ -16,6 +16,8 @@ public class Pessoa {
     private String cpf;
     private String telefoneCelular;
 
+    protected String nomeVisibilidade;
+    
     public Pessoa() {
     
     }
--- src/main/java/com/rian/cursojava/aula39/test/Aluno.java
+++ src/main/java/com/rian/cursojava/aula39/test/Aluno.java
@@ -2,52 +2,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.rian.cursojava.aula38;
+package com.rian.cursojava.aula39.test;
+
+import com.rian.cursojava.aula39.Pessoa;
+import com.rian.cursojava.aula39.Pessoa;
 
 /**
  *
  * @author rhezequias
  */
 public class Aluno extends Pessoa {
-    
+
     private String curso;
     private double notas;
-    
+
+    public void verificarAcesso() {
+
+        super.nomeVisibilidade = "Rian";
+
+        this.nomeVisibilidade = "Henrique";
+
+    }
+
     public Aluno() {
     }
-    
+
     public Aluno(String nome, String endereco, String telefone, String curso) {
         super(nome, endereco, telefone);
-        this.curso = curso;        
+        this.curso = curso;
     }
-    
+
     public String getCurso() {
         return curso;
     }
-    
+
     public void setCurso(String curso) {
         this.curso = curso;
     }
-    
+
     public double getNotas() {
         return notas;
     }
-    
+
     public void setNotas(double notas) {
         this.notas = notas;
     }
-    
+
     public double calcularMedia() {
         return 0;
     }
-    
+
     public boolean verificarAprovado() {
         return true;
     }
-    
+
     public void metodoQualquer() {
         super.setCpf("123456789101");
         this.setCpf("98765431101");
     }
-    
+
 }
