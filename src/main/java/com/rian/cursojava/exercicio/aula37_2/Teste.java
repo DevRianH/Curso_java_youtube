6fa4fabe5ef8a5c7e595535f49552e108da850d4
Rian H. S. Ezequias <rianhenrique0408@gmail.com>
2024-07-03 19:35:11 -0300

    subir git-hub 03/07/24

--- src/main/java/com/rian/cursojava/exercicio/aula37_2/Teste.java
+++ src/main/java/com/rian/cursojava/exercicio/aula37_2/Teste.java
@@ -2,19 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.rian.cursojava.aula24;
+package com.rian.cursojava.exercicio.aula37_2;
 
 /**
  *
  * @author rhezequias
  */
-public class Carro {
-
-    public String marca;
-    public String modelo;
-    public int numPassageiros;
-    public double capCombustivel;
-    public double consumoCombustivel;
-    
+public class Teste {
     
 }
