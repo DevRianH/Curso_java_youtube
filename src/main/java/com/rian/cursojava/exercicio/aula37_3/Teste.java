a0b32462081a868e36a19f125e5d86a35a0db0d8
Rian H. S. Ezequias <rhezequias@coderp.ribeiraopreto.sp.gov.br>
2024-07-10 12:54:38 -0300

    ok

--- src/main/java/com/rian/cursojava/exercicio/aula37_3/Teste.java
+++ src/main/java/com/rian/cursojava/exercicio/aula37_3/Teste.java
@@ -16,12 +16,18 @@ public class Teste {
         Peixe tubarao = new Peixe("Barbatanas e cauda", "Tubarão", 300, 0, "Cinza", "Mar", "1.5");
         Animal ursoDoCanada = new Animal("Urso-do-Canadá", 180, 4, "Vermelho", "Terra", "0.5");
 
-        System.out.println("Zoo:\n--------------------------------------------");
-        System.out.println(((Mamifero) camelo).toString());
-        System.out.println("--------------------------------------------");
-        System.out.println(((Peixe) tubarao).toString());
-        System.out.println("--------------------------------------------");
-        System.out.println(ursoDoCanada.toString());
+        Animal[] animais = new Animal[3];
+        animais[0] = camelo;
+        animais[1] = tubarao;
+        animais[2] = ursoDoCanada;
+
+        System.out.println("Zoo:\n----------------------");
+
+        for (Animal animal : animais) {
+            System.out.println(animal);
+            System.out.println("----------------------");
+
+        }
     }
 
 }
