6a15c9e652d948af70b2a7a3df16fb06b0e7cb02
Rian H. S. Ezequias <rhezequias@coderp.ribeiraopreto.sp.gov.br>
2024-07-10 13:56:39 -0300

    10/07/24

---
+++ src/main/java/com/rian/cursojava/aula44/Ave.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rian.cursojava.aula44;
+
+/**
+ *
+ * @author rhezequias
+ */
+public class Ave {
+    
+}
